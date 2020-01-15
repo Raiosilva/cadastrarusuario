b77f3d4ac0bf483e40ffd49cf7b49aa89cae7768
Paulo Arcenio <pauloarcenio.cs@gmail.com>
2020-01-14 23:02:32 -0300

    Projeto Criado

---
+++ src/test/java/com/spring/cadastrarusuario/CadastrarusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cadastrarusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrarusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
